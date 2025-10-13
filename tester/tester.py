23a12c4892197608686e82d31f7b921cefa9514d
Mohamed Houssas <mhoussas26@gmail.com>
2025-10-13 18:36:39 +0100

    replace '**' to '^' in all code; refactor the tester

---
+++ tester/tester.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("..")
+from evaluate_postfix import evaluate_postfix
+from infix_to_postfix import infix_to_postfix
+from parsing import parsing
+from math import *
+import csv
+
+
+with open("test.csv") as file:
+	csv_reader = csv.reader(file)
+
+	header = next(csv_reader)
+
+	i = 1
+	for eq, value in csv_reader:
+		output = evaluate_postfix(infix_to_postfix(parsing(eq)))
+		if float(value) != round(float(output), 6):
+			print(f"Test [{i}]: {eq}")
+			print(f"Found: {output}; Expect: {value}")
+			exit(1)
+		i += 1
+
+print("!!! All Tests Are Passed !!!")
