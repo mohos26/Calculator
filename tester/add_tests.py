19f18cce3b9edb5c06d219cdaf558bd1a15f49da
Mohamed Houssas <mhoussas26@gmail.com>
2025-10-14 21:44:59 +0100

    optimaze add_tests file

--- tester/add_tests.py
+++ tester/add_tests.py
@@ -1,4 +1,5 @@
 from math import *
+from math import factorial as fac
 
 
 with open("test.txt") as file_in, open("test.csv", "a") as file_out:
@@ -8,6 +9,7 @@ with open("test.txt") as file_in, open("test.csv", "a") as file_out:
 			break
 		if '=' in line:
 			line = line.split('=')
+			line[0] = line[0].replace("^", "**")
 			file_out.write(line[0].replace(' ', '') + ',' + line[1] + '\n')
 		else:
 			file_out.write(line.replace(' ', '') + ',' + str(round(eval(line.replace(' ', '')), 6)) + '\n')
